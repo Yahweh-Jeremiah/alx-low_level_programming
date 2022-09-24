7ebc974ca69c2408a26f2db188a3ddccfca9c5a0
Yahweh-Jeremiah <omoakor@gmail.com>
2022-09-23 18:54:08 -0700

    C-Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Whether in word or in code
+ * 
+ * Do all in the Name of Jesus
+ *
+ * Giving thanks to the Father through Him\
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
