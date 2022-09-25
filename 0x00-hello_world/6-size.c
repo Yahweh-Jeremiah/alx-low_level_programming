8bec6142c2d424b12be1c954918322be5bad0054
YAhweh-Jeremiah <omoakor@gmail.com>
2022-09-25 01:33:25 +0000

    C-Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long int longinttype;
 	float floattype
 
-	printf(stderr, "Size of a char: %d\n", sizeof(chartype));
-	printf(stderr, "Size of a int: %d\n", sizeof(inttype));
-	printf(stderr, "Size of a long int: %d\n", sizeof(longinttype));
-	printf(stderr, "Size of a long long int: %d\n", sizeof(longlonginttype));
-	printf(stderr, "Size of a float: %d\n", sizeof(floattype));
+	printf("Size of a char: %d\n", sizeof(chartype));
+	printf("Size of a int: %d\n", sizeof(inttype));
+	printf("Size of a long int: %d\n", sizeof(longinttype));
+	printf("Size of a long long int: %d\n", sizeof(longlonginttype));
+	printf("Size of a float: %d\n", sizeof(floattype));
 
 	return (0);
 }
